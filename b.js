64577a2ada501f33dd8c33f346980d30fe2ea983
Ramesh Sundar <s.rameshsundar23@gmail.com>
2014-06-05 01:03:38 +0530

    this is b.js

---
+++ b.js
@@ -0,0 +1,9 @@
+<script id="entry-template" type="text/x-handlebars-template">
+  <div class="row">
+  <div class="col-lg-6">
+  image
+  </div>
+  <div class="col-lg-6"></div>
+  text
+  </div>
+</script>
