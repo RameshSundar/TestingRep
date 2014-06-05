e7d5be90769ac54a26b5ea9992c2c14677f1fef3
Ramesh Sundar <s.rameshsundar23@gmail.com>
2014-06-05 18:01:12 +0530

    not reset the a.js

--- a.js
+++ a.js
@@ -5,3 +5,11 @@
   
   </div>
 </script>
+<script id="entry-template" type="text/x-handlebars-template">
+  <div class="row">
+  <div class="col-lg-6"></div>
+  <div class="col-lg-6"></div>
+  
+  </div>
+</script>
+
