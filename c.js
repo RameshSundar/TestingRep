cc40ebae744be66ab51e4d7052b637a71327a32e
Ramesh Sundar <s.rameshsundar23@gmail.com>
2014-06-05 16:41:52 +0530

    Alter the div

---
+++ c.js
@@ -0,0 +1,13 @@
+<div class="row"> 
+<div class="col-lg-6">
+<h2>It will print Header</h2>
+</div>
+
+<div class="col-lg-3">
+<p> It will print Paragrapg</p> 
+<div class="col-lg-3">
+<p> It will print Paragrapg</p> 
+</div>
+
+
+</div>
