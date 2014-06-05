8ba506fa22e69b609ca145e98478d62e794c097a
Ramesh Sundar <s.rameshsundar23@gmail.com>
2014-06-05 17:27:35 +0530

    add extra Row

--- c.js
+++ c.js
@@ -11,3 +11,18 @@
 
 
 </div>
+
+<div class="row"> 
+<div class="col-lg-6">
+<h2>It will print Header</h2>
+</div>
+
+<div class="col-lg-3">
+<p> It will print Paragrapg</p> 
+<div class="col-lg-3">
+<p> It will print Paragrapg</p> 
+</div>
+
+
+</div>
+
