c1f392c6568d570d1920bbe37d914c7ca775947e
Ramesh Sundar <s.rameshsundar23@gmail.com>
2014-06-05 18:14:49 +0530

    This is for de

---
+++ d.js
@@ -0,0 +1,7 @@
+<script id="Headingtemplate" type="text/x-handlebars-template">
+  template content
+</script>
+
+<script id="Sidebartemplate" type="text/x-handlebars-template">
+  template content
+</script>
